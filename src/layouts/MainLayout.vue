c28916a5142c6a5d8d590a79b7939187cba27310
Ronnie1320 <68396806+Ronnie1320@users.noreply.github.com>
2022-07-26 09:42:59 +0200

    Formatted (Prettier)

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,18 +2,18 @@
   <q-layout view="lHh Lpr fFf">
     <q-header elevated>
       <q-toolbar>
+        <q-toolbar-title> Quasar App </q-toolbar-title>
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <q-btn color="black" :label="btn_text" @click="dark_mode()" style="margin-right:15px"/>
+        <q-btn
+          color="black"
+          :label="btn_text"
+          @click="dark_mode()"
+          style="margin-right: 15px"
+        />
 
         <div>&copy; Ren&eacute; Regensbogen 2022</div>
       </q-toolbar>
     </q-header>
-
-
 
     <q-page-container>
       <router-view />
@@ -22,25 +22,19 @@
 </template>
 
 <script lang="ts" setup>
-import {ref} from 'vue';
+import { ref } from 'vue';
 import { useQuasar } from 'quasar';
 
 name: 'MainLayout';
 
-
-    const $q = useQuasar()
-    var btn_text = ref('Black');
-    function dark_mode() {
-     $q.dark.toggle();
-     if (btn_text.value == 'Black') {
-       btn_text.value = 'White'
-     } else {
-       btn_text.value = 'Black'
-     }
-     }
-
-
-
-
-
+const $q = useQuasar();
+var btn_text = ref('Black');
+function dark_mode() {
+  $q.dark.toggle();
+  if (btn_text.value == 'Black') {
+    btn_text.value = 'White';
+  } else {
+    btn_text.value = 'Black';
+  }
+}
 </script>
