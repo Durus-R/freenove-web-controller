97f3ff75f0a9cd273ca4cd0658525b61e8d1d600
ronnie1320 <smartpower0105@gmail.com>
2022-07-28 15:43:53 +0200

    Added flexbos

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,14 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <q-img :src="image_file" />
+  <q-page class="fit row items-start justify-center">
+    <div id='camera_window' style="overflow: auto; min-height: 600px; max-height: 600px;  min-width: 800px; max-width: 800px;">
+      <q-img :src="image_file" />
+    </div>
+    <div id="led_control" style="overflow: auto; min-height: 600px; max-height: 600px;  min-width: 400px; max-width: 400px;"></div>
+    <div id="sensor_values" style="overflow: auto; min-width: 200px; max-width: 200px;"></div>
+    <div id="motor_control" class="offset-1" style="overflow: auto; min-height: 400px; max-height: 400px;  min-width: 400px; max-width: 400px;"></div>
+    <div id="servo_control" style="overflow: auto; min-height: 400px; max-height: 400px;  min-width: 400px; max-width: 400px;"></div>
+    <div id="misc" style="overflow: auto;"></div>
+
   </q-page>
 </template>
 
@@ -19,7 +27,6 @@ watch(
     } else {
       image_file.value = 'freenove_orange.png';
     }
-    console.log('change');
   }
 );
 </script>
